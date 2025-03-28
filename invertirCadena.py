1fb5f6d23fb42a003afcfca8b33dbae7b813284d
saulcv87 <saulcv87@gmail.com>
2025-03-28 17:08:57 +0100

    invertirCadena

--- invertirCadena.py
+++ invertirCadena.py
@@ -1,3 +1,6 @@
+#   Crea un programa que invierta el orden de una cadena de texto
+#   sin usar funciones propias del lenguaje que lo hagan de forma automática.
+#   - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
 
 s = 'Hola mundo'
 print(s)
